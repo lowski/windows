0e7f4f6a4750360d90c715b7d97e22abbc1b1703
Leonard von Lojewski <dev@vonlojewski.me>
2022-06-01 17:23:01 +0200

    refactor: store config under ~/.config/
    The sqlite files will now be stored at ~/.config/windows/storage.sqlite

--- windows/CoreDataManager.swift
+++ windows/CoreDataManager.swift
@@ -15,6 +15,18 @@ class CoreDataManager {
     
     private lazy var persistentContainer: NSPersistentContainer = {
         let container = NSPersistentContainer(name: "windows")
+        
+        let homeDirectory = FileManager.default.homeDirectoryForCurrentUser
+        let url = homeDirectory.appendingPathComponent(".config/windows/storage.sqlite")
+        do {
+            try? FileManager.default.createDirectory(at: url.deletingLastPathComponent(), withIntermediateDirectories: true)
+        }
+        
+        let description = NSPersistentStoreDescription(url: url);
+        description.shouldInferMappingModelAutomatically = true
+        description.shouldMigrateStoreAutomatically = true
+        container.persistentStoreDescriptions = [description]
+        
         container.loadPersistentStores {(_, error) in
             _ = error.map{ fatalError("Unresolved error \($0)") }
         }
